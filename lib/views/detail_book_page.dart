7fbce3b00e7997953af97315e1afe7c6c1c303c5
ihsan.satriawan <ihsan.satriawan@BA-00021650.local>
2022-06-17 09:15:01 +0700

    Show data book detail and show image screen view

--- lib/views/detail_book_page.dart
+++ lib/views/detail_book_page.dart
@@ -1,21 +1,42 @@
+import 'dart:convert';
+
+import 'package:book_app/models/book_detail_response.dart';
+import 'package:book_app/views/image_view_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:http/http.dart' as http;
 
 class DetailBookPage extends StatefulWidget {
-  const DetailBookPage({Key? key}) : super(key: key);
+  const DetailBookPage({Key? key, required this.isbn}) : super(key: key);
+
+  final String isbn;
 
   @override
   State<DetailBookPage> createState() => _DetailBookPageState();
 }
 
 class _DetailBookPageState extends State<DetailBookPage> {
-  fetchBookApi() async {
-    var url = Uri.parse('https://api.itbook.store/1.0/new');
+  BookDetailResponse? detailBook;
+  fetchDetailBookApi() async {
+    print(widget.isbn);
+    var url = Uri.parse('https://api.itbook.store/1.0/books/${widget.isbn}');
     var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final jsonDetail = jsonDecode(response.body);
+      detailBook = BookDetailResponse.fromJson(jsonDetail);
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchDetailBookApi();
   }
 
   @override
@@ -24,6 +45,50 @@ class _DetailBookPageState extends State<DetailBookPage> {
       appBar: AppBar(
         title: Text("Detail"),
       ),
+      body: detailBook == null
+          ? Center(child: const CircularProgressIndicator())
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ImageViewScreen(imageUrl: detailBook!.image!),
+                          ),
+                        );
+                      },
+                      child: Image.network(
+                        detailBook!.image!,
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(detailBook!.title!),
+                          Text(detailBook!.subtitle!),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Text(detailBook!.price!),
+                Text(detailBook!.isbn10!),
+                Text(detailBook!.isbn13!),
+                Text(detailBook!.pages!),
+                Text(detailBook!.authors!),
+                Text(detailBook!.publisher!),
+                Text(detailBook!.desc!),
+                Text(detailBook!.rating!),
+              ],
+            ),
     );
   }
 }
